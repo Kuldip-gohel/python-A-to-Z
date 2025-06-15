c28662297e1aaea2d361ca26dd805065e7503fc7
Kuldip-gohel <kuldipgohel11@gmail.com>
2025-06-15 20:23:16 +0530

    Add files via upload

---
+++ python basic to advance/python fundamentals/Comments.py
@@ -0,0 +1,27 @@
+'''Comments are lines in Python code that are not executed.
+They are used to:
+Explain what the code does
+Make code easier to understand
+Help with debugging or planning
+'''
+
+# There are two types of comments in Python:
+
+# 1.Single-line comment → uses #
+# 2.Multi-line comment → uses triple quotes ''' or """
+
+# Some examples of uses comments.
+
+# single line comment
+a = 10
+b = 20
+print("Multiplication of a and b is -->", a * b) #that do multiplication of a and b
+
+#Multiline commemt.
+''' This is a basic calculater that perform basic operation like add, multipication, division and substraction'''
+a1 = 20
+a2 = 30
+print("addition is -->", a1+a2)
+print("Multiplication is -->", a1*a2)
+print("division is -->", a1/a2)
+print("substraction is -->", a1-a2)
