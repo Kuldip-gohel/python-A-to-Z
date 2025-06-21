5b16cdbf31ab091a08357852f2f42b71579e5812
Kuldip-gohel <kuldipgohel11@gmail.com>
2025-06-21 17:32:32 +0530

    Add files via upload

---
+++ python basic to advance/String/strings.py
@@ -0,0 +1,16 @@
+'''
+A string is a sequence of characters enclosed in quotes.
+It can contain letters, numbers, symbols, and spaces.
+
+You can create a string using:
+ Single quotes 'Hello'
+ Double quotes "Hello"
+''' 
+#  Triple quotes for multi-line strings '''Hello''' or """Hello"""
+
+# Example
+ 
+name1 = 'bhem'
+name2 = "chutki"
+name3 = ''' They bot are friends '''
+
